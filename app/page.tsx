c42b4e26a9e3af6161ea078aca78a88e0898e2ac
prpjzz <groovemusic4399@pm.me>
2025-04-05 23:45:18 +0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          <h1>Nguyen Huu Phuoc Hello World ~</h1>
+        </ol>
+
+      </main>
+    </div>
+  );
+}
